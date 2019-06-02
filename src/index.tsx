7543e049486a1339659ae1375527fb8427338de4
wangyang65 <wangyang65@baidu.com>
2019-06-02 16:21:46 +0800

    feat add rem&ThemeLoader support

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'utils/rem';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import {ThemeProvider} from '@baidu/rubik-ui';
-import theme from './theme';
+import ThemeLoader from './theme/ThemeLoader';
 
+const theme = ThemeLoader.getThemeConfig('green');
 const render = Component => {
     ReactDOM.render(
         <Provider store={store}>
--- src/theme/palette.tsx
+++ src/theme/palette.tsx
@@ -17,4 +17,56 @@ const palette: {[key: string]: string} = {
     cPurpleGray: 'rgba(126,132,172,1)'
 };
 
+export const green = {
+    text: '#333333',
+    primary: '#19A97B',
+    primaryNormal: '#19A97B',
+    primaryNormalShadow: 'rgba(25,169,123,0.30)',
+    primaryHover: '#47BA95',
+    primaryHoverShadow: 'rgba(131,188,169,0.40)',
+    primaryActive: '#148762',
+    primaryActiveShadow: 'rgba(25,169,123,0.30)',
+    primarySelected: '#19A97B',
+    // pie chart color
+    primaryPie0: '#019163',
+    primaryPie1: '#19A97B',
+    // bar chart color
+    primaryBar0: '#019163',
+    primaryBar1: '#19A97B',
+    primaryBar2: '#5ED7B0',
+    primaryBar3: '#B9EADA'
+};
+export const blue = {
+    text: '#333333',
+    primary: '#0E99F6',
+    primaryNormal: '#0E99F6',
+    primaryHover: '#3EADF7',
+    primaryActive: '#0B7AC4',
+    primarySelected: '#0E99F6',
+    // pie chart color
+    primaryPie0: '#28D3FF',
+    primaryPie1: '#2DA9E8',
+    // bar chart color
+    primaryBar0: '#0D6ABC',
+    primaryBar1: '#1995E8',
+    primaryBar2: '#65CBFF',
+    primaryBar3: '#96E9FF'
+};
+
+export const black = {
+    text: '#333333',
+    primary: '#1F2836',
+    primaryNormal: '#9BA9C0',
+    primaryHover: '#000B1D',
+    primaryActive: '#000B1D',
+    primarySelected: '#1F2836',
+    // pie chart color
+    primaryPie0: '#9BA9C0',
+    primaryPie1: '#59677C',
+    // bar chart color
+    primaryBar0: '#1F2837',
+    primaryBar1: '#5B6F90',
+    primaryBar2: '#9EAFCC',
+    primaryBar3: '#D3DEF0'
+};
 export default palette;
