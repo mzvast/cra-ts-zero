5d45f9dc8dc8da4360d9e4abf17e7b5b577877e4
mzvast <mzvast@gmail.com>
2021-02-25 20:40:48 +0800

    Update PrivateRoute.tsx

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -4,33 +4,34 @@
  * @email [mzvast@gmail.com]
  * @create date 2019-03-13 16:40:17
  */
-/* eslint-disable max-len,operator-linebreak,space-before-function-paren */
+/* eslint-disable max-len,babel/new-cap,operator-linebreak,fecs-export-on-declare,space-before-function-paren */
 
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
-import config from 'config';
+import Config from 'config';
 import AuthGuard from 'api/AuthGuard';
+// import FallbackErrorPage from '../routes/error-page/FallbackErrorPage';
 // https://tylermcginnis.com/react-router-protected-routes-authentication/
 const PrivateRoute = ({component: Component, ...rest}) => {
-    if (!AuthGuard.isAuthenticated) {
-        // window.history.back(); // fixme: 如果空降无权限页会遣返
-        return <Redirect to={config.path.root} />;
-    }
-
-    function renderFn(props: any) {
-        // console.log('renderFn::rest.path', rest.path);
-        return AuthGuard.isAuthenticated ? (
-            <Component {...props} />
-        ) : (
-            <Redirect
-                to={{
-                    pathname: config.path.root,
-                    state: {from: props.location}
-                }}
-            />
-        );
-    }
-
-    return <Route {...rest} render={renderFn} />;
+    // if (!window.navigator.onLine) {
+    //     return <FallbackErrorPage />;
+    // }
+    return (
+        <Route
+            {...rest}
+            render={(props: any) =>
+                AuthGuard.isAuthenticated ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: Config.path.welcome,
+                            state: {from: props.location}
+                        }}
+                    />
+                )
+            }
+        />
+    );
 };
 export default PrivateRoute;
